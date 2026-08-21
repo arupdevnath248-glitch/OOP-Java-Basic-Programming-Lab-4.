aa77d5469209ef99c71c0eaf5ad0de160e23d09b
arupdevnath248-glitch <arupdevnath248@gmail.com>
2026-08-21 19:09:08 +0600

    new

---
+++ for_each_loop_ex2.java
@@ -0,0 +1,9 @@
+public class for_each_loop_ex2 {
+  public static void main(String[] args) {
+    int[] numbers = {10, 20, 30, 40};
+
+    for (int num : numbers) {
+      System.out.println(num);
+    }
+  }
+}
